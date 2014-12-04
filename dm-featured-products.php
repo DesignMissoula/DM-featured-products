1046a180704071c85e4453c17899791ede3346b8
Bradford Knowlton <brad@ucanbonline.net>
2014-12-03 19:17:39 -0700

    initial commit

---
+++ dm-featured-products.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Featured Products
+Plugin URI: http://www.designmissoula.com/
+Description: Used by Millions to make WordPress better.
+Author: Bradford Knowlton
+Version: 1.9.9
+Author URI: http://bradknowlton.com/
+GitHub Plugin URI: https://github.com/DesignMissoula/DM-featured-products
+GitHub Branch: master
+*/
+
